b2040fe756752af4ef433b9c2d2772921edba6d5
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-01 11:00:53 -0500

    Mejoras en el mensaje que se muestra en el todoCounter

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { TodoContext } from '../TodoContext';
 import './TodoCounter.css';
+import { FcApproval } from "react-icons/fc";
 
 function TodoCounter() {
   const { totalTodos, completedTodos } = React.useContext(TodoContext);
   
   return (
-    <h2 className="TodoCounter">Has completado {completedTodos} de {totalTodos} TODOs</h2>
+    <h2 className="TodoCounter">
+      Has completado 
+      <small className="text-muted"> {completedTodos} de {totalTodos} </small>
+      TO DOs <FcApproval />
+    </h2>
   );
 }
 
